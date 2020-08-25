a8cccc58f3214ed6faf99d841620fb257c6442f1
Galocg <korocg@naver.com>
2020-08-26 00:42:52 +0900

    4.2.2.3 버튼을 누를 때 comment 값을 공백으로 설정

---
+++ chapter4/4.2.2.3-1.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class EventPractice extends Component{
+    state = {
+        message: ''
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>이벤트 연습</h1>
+                <input 
+                    type="text"
+                    name="message"
+                    placeholder="아무거나"
+                    value={this.state.message}
+                />
+            </div>
+        );
+    }
+}
+
+export default EventPractice;
