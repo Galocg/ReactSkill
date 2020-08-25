13e4c56773df4d63339e6c209fe6c6c6aa33a50d
Galocg <korocg@naver.com>
2020-08-26 02:39:38 +0900

    4.2.5-1 onKeyPress 이벤트 핸들링

--- react_exec/src/EventPractice.js
+++ react_exec/src/EventPractice.js
@@ -1,35 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class EventPractice extends Component{
     state = {
-        message: ''
+        username : '',
+        message : ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+
+    handleClick = () => {
+        alert(this.state.username + ': ' + this.state.message);
+        this.setState({
+            username: '',
+            message: ''
+        });
     }
 
     render(){
         return (
             <div>
                 <h1>이벤트 연습</h1>
-                <input 
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="사용자명"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                />
+                <input
                     type="text"
                     name="message"
                     placeholder="아무거나"
                     value={this.state.message}
-                    onChange={
-                        (e)=>{
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    onChange={this.handleChange}
                 />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
