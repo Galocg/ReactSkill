0f4fe56286e993f799503bd1f4247075669597fb
Galocg <korocg@naver.com>
2020-08-29 01:29:43 +0900

    10.3.32 todos 배열에서 id로 항목 지우기

---
+++ chapter10/22.js
@@ -0,0 +1,31 @@
+import React, { useState, useRef, useCallback } from ‘react‘;
+import TodoTemplate from ‘./components/TodoTemplate‘;
+import TodoInsert from ‘./components/TodoInsert‘;
+import TodoList from ‘./components/TodoList‘;
+
+
+const App = () => {
+  (…)
+
+
+
+const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id != = id));
+    },
+    [todos],
+  );
+
+
+
+return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} />
+    </TodoTemplate>
+  );
+};
+
+
+
+export default App;
