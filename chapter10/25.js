a32c405f764df6eded1056e42be78e4cfe2f3585
Galocg <korocg@naver.com>
2020-08-29 01:31:08 +0900

    10.3.4.1-1 onToggle 구현하기

---
+++ chapter10/25.js
@@ -0,0 +1,28 @@
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+ 
+const App = () => {
+  (...)
+ 
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+ 
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+ 
+export default App;
