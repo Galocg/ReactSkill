7a00e7ff2303ae3a100fd8dfe36ddb76870137a9
Galocg <korocg@naver.com>
2020-08-30 02:55:07 +0900

    13.6.2-2 withRouter

---
+++ chapter13/17.js
@@ -0,0 +1,18 @@
+import React from ‘react‘;
+import { Link, Route } from ‘react-router-dom‘;
+import Profile from ‘./Profile‘;
+import WithRouterSample from ‘./WithRouterSample‘;
+
+
+const Profiles = () => {
+  return (
+    <div>
+      (…)
+      <WithRouterSample />
+    </div>
+  );
+};
+
+
+
+export default Profiles;
