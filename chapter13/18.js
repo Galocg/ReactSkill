2a5d70aaa69a5cda31cef27acc5fe30b11551a08
Galocg <korocg@naver.com>
2020-08-30 02:55:30 +0900

    13.6.2-3 withRouter

---
+++ chapter13/18.js
@@ -0,0 +1,26 @@
+import React from ‘react‘;
+import { withRouter } from ‘react-router-dom‘;
+import WithRouterSample from ‘./WithRouterSample‘;
+
+
+(…)
+
+
+
+const Profile = ({ match }) => {
+  const { username } = match.params;
+  const profile = data[username];
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다.</div>;
+  }
+  return (
+    <div>
+      (…)
+      <WithRouterSample />
+    </div>
+  );
+};
+
+
+
+export default withRouter(Profile);
