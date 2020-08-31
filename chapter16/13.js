f2b4c0661536c076c340cec0058dfec82a8ddb04
Galocg <korocg@naver.com>
2020-09-01 00:26:37 +0900

    16.2.10-1 액션 발생시키기

---
+++ chapter16/13.js
@@ -0,0 +1,10 @@
+(…)
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitch());
+};
+btnIncrease.onclick = () => {
+  store.dispatch(increase(1));
+};
+btnDecrease.onclick = () => {
+  store.dispatch(decrease());
+};
