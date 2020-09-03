5fcfd204875b050987d6c41cb95dc7ee1d18741f
Galocg <korocg@naver.com>
2020-09-03 23:56:45 +0900

    17.6.1.1-1 counter 모듈에 적용하기

---
+++ chapter17/27.js
@@ -0,0 +1,9 @@
+import { createAction } from 'redux-actions';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+(...)
