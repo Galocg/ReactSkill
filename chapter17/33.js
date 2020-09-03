705b44fbe43b2d1866235cec46f719cdfef728bb
Galocg <korocg@naver.com>
2020-09-03 23:59:12 +0900

    17.7.1-1 useSelector로 상태 조회하기

---
+++ chapter17/33.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  return <Counter number={number} />;
+};
+
+export default CounterContainer;
