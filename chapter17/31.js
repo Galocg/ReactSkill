1e128d408bb92189f751b4e108caefc511705131
Galocg <korocg@naver.com>
2020-09-03 23:58:19 +0900

    17.6.1.2-3 todos 모듈에 적용하기

---
+++ chapter17/31.js
@@ -0,0 +1,23 @@
+(...)
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map(todo =>
+        todo.id = = = id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter(todo => todo.id != = id),
+    }),
+  },
+  initialState,
+);
+
+export default todos;
