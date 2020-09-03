ec02cb99e294202b6151c626e71e5cef1b742592
Galocg <korocg@naver.com>
2020-09-04 00:57:41 +0900

    20.3.2-6 서버 사이드 렌더링 전용 웹팩 환경 설정 작성하기

---
+++ chapter20/17.js
@@ -0,0 +1,21 @@
+const nodeExternals = require('webpack-node-externals');
+const paths = require('./paths');
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+const webpack = require('webpack');
+const getClientEnvironment = require('./env');
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+const publicUrl = paths.servedPath.slice(0, -1);
+const env = getClientEnvironment(publicUrl);
+
+module.exports = {
+  (...)
+  externals: [nodeExternals()],
+  plugins: [
+    new webpack.DefinePlugin(env.stringified) // 환경변수를 주입해 줍니다.
+  ]
+};
