b2df7efae3d1e1845096069b848b149e75037b41
Galocg <korocg@naver.com>
2020-09-04 00:55:52 +0900

    20.3.2-1 서버 사이드 렌더링 전용 웹팩 환경 설정 작성하기

---
+++ chapter20/12.js
@@ -0,0 +1,23 @@
+(…)
+module.exports = {
+  dotenv: resolveApp(‘.env‘),
+  appPath: resolveApp(‘.‘),
+  appBuild: resolveApp(‘build‘),
+  appPublic: resolveApp(‘public‘),
+  appHtml: resolveApp(‘public/index.html‘),
+  appIndexJs: resolveModule(resolveApp, ‘src/index‘),
+  appPackageJson: resolveApp(‘package.json‘),
+  appSrc: resolveApp(‘src‘),
+  appTsConfig: resolveApp(‘tsconfig.json‘),
+  yarnLockFile: resolveApp(‘yarn.lock‘),
+  testsSetup: resolveModule(resolveApp, ‘src/setupTests‘),
+  proxySetup: resolveApp(‘src/setupProxy.js‘),
+  appNodeModules: resolveApp(‘node_modules‘),
+  publicUrl: getPublicUrl(resolveApp(‘package.json‘)),
+  servedPath: getServedPath(resolveApp(‘package.json‘)),
+  ssrIndexJs: resolveApp(‘src/index.server.js‘), // 서버 사이드 렌더링 엔트리
+  ssrBuild: resolveApp(‘dist‘) // 웹팩 처리 후 저장 경로
+};
+
+
+module.exports.moduleFileExtensions = moduleFileExtensions;
