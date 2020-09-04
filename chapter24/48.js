4c75dca363ac556b7832f36acae7b49e64fff1d9
Galocg <korocg@naver.com>
2020-09-04 22:12:00 +0900

    24.2.4-5 회원가입 구현

---
+++ chapter24/48.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, register } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
+
+const RegisterForm = ({ history }) => {
+  (...)
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+    }
+  }, [history, user]);
+
+  return (...);
+};
+
+export default withRouter(RegisterForm);
