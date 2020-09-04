854c274eae77026f089d6439cc87ddef3cccd63e
Galocg <korocg@naver.com>
2020-09-04 22:16:11 +0900

    24.3.2.2-1 로그인 상태 유지하기

---
+++ chapter24/58.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from ‘react‘;
+import { useSelector, useDispatch } from ‘react-redux‘;
+import { withRouter } from ‘react-router-dom‘;
+import { changeField, initializeForm, login } from ‘../../modules/auth‘;
+import AuthForm from ‘../../components/auth/AuthForm‘;
+import { check } from ‘../../modules/user‘;
+
+
+const LoginForm = ({ history }) => {
+(…)
+
+
+
+useEffect(() => {
+    if (user) {
+      history.push(‘/‘);
+      try {
+        localStorage.setItem(‘user‘, JSON.stringify(user));
+      } catch (e) {
+        console.log(‘localStorage is not working‘);
+      }
+    }
+  }, [history, user]);
+
+
+
+return (…);
+};
+
+
+
+export default withRouter(LoginForm);
