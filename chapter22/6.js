0efa20e3b34e102f9830f6da4ab3b58467dbbb09
Galocg <korocg@naver.com>
2020-09-04 21:05:46 +0900

    22.4.1-1 기존 코드 ES Module 형태로 바꾸기

---
+++ chapter22/6.js
@@ -0,0 +1,30 @@
+(…)
+export const write = ctx => {
+  (…)
+};
+
+
+export const list = ctx => {
+   (…)
+};
+export const read = ctx => {
+   (…)
+};
+
+
+
+export const remove = ctx => {
+   (…)
+};
+
+
+
+export const replace = ctx => {
+   (…)
+};
+
+
+
+export const update = ctx => {
+   (…)
+};
