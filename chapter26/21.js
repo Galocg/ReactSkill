1a4424a8833383b63cb65723997704f69fb5b243
Galocg <korocg@naver.com>
2020-09-06 03:20:42 +0900

    26.2.3-3 HTML 필터링하기

---
+++ chapter26/21.js
@@ -0,0 +1,34 @@
+import Post from ‘../../models/post‘;
+import mongoose from ‘mongoose‘;
+import Joi from ‘joi‘;
+import sanitizeHtml from ‘sanitize-html‘;
+
+
+const { ObjectId } = mongoose.Types;
+
+
+
+const sanitizeOption = {
+  allowedTags: [
+    ‘h1‘,
+    ‘h2‘,
+    ‘b‘,
+    ‘i‘,
+    ‘u‘,
+    ‘s‘,
+    ‘p‘,
+    ‘ul‘,
+    ‘ol‘,
+    ‘li‘,
+    ‘blockquote‘,
+    ‘a‘,
+    ‘img‘,
+  ],
+  allowedAttributes: {
+    a: [‘href‘, ‘name‘, ‘target‘],
+    img: [‘src‘],
+    li: [‘class‘],
+  },
+  allowedSchemes: [‘data‘, ‘http‘],
+};
+(…)
