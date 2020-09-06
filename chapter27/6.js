c1445fa3160b3f5edfa47641766b06f54f634b43
Galocg <korocg@naver.com>
2020-09-07 00:40:46 +0900

    27.1.2-4 수정 버튼 클릭 시 글쓰기 페이지로 이동하기

---
+++ chapter27/6.js
@@ -0,0 +1,56 @@
+(…)
+const Editor = ({ title, body, onChangeField }) => {
+  const quillElement = useRef(null); // Quill을 적용할 DivElement를 설정
+  const quillInstance = useRef(null); // Quill 인스턴스를 설정
+
+
+useEffect(() => {
+    quillInstance.current = new Quill(quillElement.current, {
+      theme: ‘bubble‘,
+      placeholder: ‘내용을 작성하세요…’,
+      modules: {
+        // 더 많은 옵션
+        // https://quilljs.com/docs/modules/toolbar/ 참고
+        toolbar: [
+          [{ header: ‘1‘ }, { header: ‘2‘ }],
+          [‘bold‘, ‘italic‘, ‘underline‘, ‘strike‘],
+          [{ list: ‘ordered‘ }, { list: ‘bullet‘ }],
+          [‘blockquote‘, ‘code-block‘, ‘link‘, ‘image‘],
+        ],
+      },
+    });
+
+
+
+const mounted = useRef(false);
+  useEffect(() => {
+    if (mounted.current) return;
+    mounted.current = true;
+    quillInstance.current.root.innerHTML = body;
+  }, [body]);
+
+
+
+const onChangeTitle = e => {
+    onChangeField({ key: ‘title‘, value: e.target.value });
+  };
+
+
+
+return (
+    <EditorBlock>
+      <TitleInput
+        placeholder=“제목을 입력하세요“
+        onChange={onChangeTitle}
+        value={title}
+      />
+      <QuillWrapper>
+        <div ref={quillElement} />
+      </QuillWrapper>
+    </EditorBlock>
+  );
+};
+
+
+
+export default Editor;
