f050fbe445ab54a51b51ef89a840dbe65b502fe8
Galocg <korocg@naver.com>
2020-09-07 00:42:54 +0900

    27.2-2 포스트 삭제

---
+++ chapter27/12.js
@@ -0,0 +1,43 @@
+import React, { useState, useCallback } from ‘react‘;
+import styled from ‘styled-components‘;
+import palette from ‘../../lib/styles/palette‘;
+import AskRemoveModal from ‘./AskRemoveModal‘;
+
+
+(…)
+
+
+
+const PostActionButtons = ({ onEdit, onRemove }) => {
+  const [modal, setModal] = useState(false);
+  const onRemoveClick = () => {
+    setModal(true);
+  };
+  const onCancel = () => {
+    setModal(false);
+  };
+  const onConfirm = () => {
+    setModal(false);
+    onRemove();
+  };
+
+
+
+return (
+    <>
+      <PostActionButtonsBlock>
+        <ActionButton onClick={onEdit}>수정</ActionButton>
+        <ActionButton onClick={onRemoveClick}>삭제</ActionButton>
+      </PostActionButtonsBlock>
+      <AskRemoveModal
+        visible={modal}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />
+    </>
+  );
+};
+
+
+
+export default PostActionButtons;
