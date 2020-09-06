4c2bddbdacc5d7c7c1c59d41ec6d53ad3627458b
Galocg <korocg@naver.com>
2020-09-07 00:40:14 +0900

    27.1.2-3 수정 버튼 클릭 시 글쓰기 페이지로 이동하기

---
+++ chapter27/5.js
@@ -0,0 +1,11 @@
+(...)
+const PostActionButtons = ({ onEdit }) => {
+  return (
+    <PostActionButtonsBlock>
+      <ActionButton onClick={onEdit}>수정</ActionButton>
+      <ActionButton>삭제</ActionButton>
+    </PostActionButtonsBlock>
+  );
+};
+
+export default PostActionButtons;
